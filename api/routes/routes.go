8ded53a10537910af34dca936ee94678f149a030
Ashish <dhakalashish18@gmail.com>
2023-07-28 18:10:50 +0545

    feat: setup route

--- api/routes/routes.go
+++ api/routes/routes.go
@@ -5,11 +5,12 @@ import "go.uber.org/fx"
 // Module exports dependency to container
 var Module = fx.Options(
 	fx.Provide(NewRoutes),
-  fx.Provide(NewTodoRoutes),
+	fx.Provide(NewTodoRoutes),
 	fx.Provide(NewUtilityRoutes),
 	fx.Provide(NewDocsRoutes),
 	fx.Provide(NewJwtAuthRoutes),
 	fx.Provide(NewUserRoutes),
+	fx.Provide(NewBackupRoutes),
 )
 
 // Routes contains multiple routes
@@ -22,18 +23,20 @@ type Route interface {
 
 // NewRoutes sets up routes
 func NewRoutes(
-	 todoRoutes TodoRoutes,
+	todoRoutes TodoRoutes,
 	utilityRoutes UtilityRoutes,
 	userRoutes UserRoutes,
 	jwtAuthRoutes JwtAuthRoutes,
 	docsRoutes DocsRoutes,
+	backupRoutes BackupRoutes,
 ) Routes {
 	return Routes{
-	 todoRoutes,
+		todoRoutes,
 		utilityRoutes,
 		jwtAuthRoutes,
 		userRoutes,
 		docsRoutes,
+		backupRoutes,
 	}
 }
 
