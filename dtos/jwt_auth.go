64d58cdb7f506ced39abfec8bad7357a5f867b5e
Ashish <dhakalashish18@gmail.com>
2023-07-28 12:03:55 +0545

    change: login dto

--- dtos/jwt_auth.go
+++ dtos/jwt_auth.go
@@ -2,6 +2,6 @@ package dtos
 
 // JWTLoginRequestData Request body data to authenticate user with jwt-auth
 type JWTLoginRequestData struct {
-	Email    string `json:"email" validate:"required"`
-	Password string `json:"password" validate:"required"`
+	EmailOrUsername string `json:"email_or_username" validate:"required"`
+	Password        string `json:"password" validate:"required"`
 }
