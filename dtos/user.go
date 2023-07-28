a50c3ec34b837f3daec163e6108f6c0a0bf9ca93
Ashish <dhakalashish18@gmail.com>
2023-07-28 12:04:04 +0545

    change: create user dto

--- dtos/user.go
+++ dtos/user.go
@@ -4,8 +4,19 @@ import "boilerplate-api/models"
 
 // CreateUserRequestData Request body data to create user
 type CreateUserRequestData struct {
-	models.User
-	ConfirmPassword string `json:"confirm_password" validate:"required"`
+	Email    string `gorm:"email" json:"email" validate:"required,email"`
+	FullName string `gorm:"full_name" json:"full_name" validate:"required"`
+	Password string `gorm:"password" json:"password" validate:"required"`
+	Username string `gorm:"username" json:"username" validate:"required"`
+}
+
+func (c *CreateUserRequestData) GetUserObj() models.User {
+	return models.User{
+		Email:    c.Email,
+		FullName: c.FullName,
+		Password: c.Password,
+		Username: c.Username,
+	}
 }
 
 // GetUserResponse Dtos for User model
