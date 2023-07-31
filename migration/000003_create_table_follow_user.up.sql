c4cedc02817b3fe56525269502b0489d1570f089
Ashish <dhakalashish18@gmail.com>
2023-07-31 19:11:22 +0545

    change: column name

--- migration/000003_create_table_follow_user.up.sql
+++ migration/000003_create_table_follow_user.up.sql
@@ -1,12 +1,11 @@
-
 CREATE TABLE IF NOT EXISTS `follow_user` (
     `id` INT NOT NULL AUTO_INCREMENT,
-    `user_id` INT NOT NULL,
-    `followed_user_id` INT NOT NULL,
+    `followed_by_id` INT NOT NULL,
+    `followed_to_id` INT NOT NULL,
     `is_approved` tinyint DEFAULT 1,
     `created_at` DATETIME NOT NULL,
     `updated_at` DATETIME NULL,
     `deleted_at` DATETIME NULL,
     PRIMARY KEY (id),
-    CONSTRAINT `UQ_user_id_followed_user_id` UNIQUE (`followed_user_id`, `user_id`)
+    CONSTRAINT `UQ_user_id_followed_by_to` UNIQUE (`followed_by_id`, `followed_to_id`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
