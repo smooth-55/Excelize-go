d564ae2df30de1ff83809e09292125b0b8319889
Ashish <dhakalashish18@gmail.com>
2023-07-31 19:11:51 +0545

    feat: FollowSuggestions dto

--- dtos/user.go
+++ dtos/user.go
@@ -21,10 +21,23 @@ func (c *CreateUserRequestData) GetUserObj() models.User {
 
 // GetUserResponse Dtos for User model
 type GetUserResponse struct {
-	models.User
-	Password string
+	ID        int64  `json:"id"`
+	Email     string `json:"email"`
+	FullName  string `json:"full_name"`
+	Phone     string ` json:"phone" `
+	Gender    string ` json:"gender"`
+	Username  string ` json:"username"`
+	IsPrivate bool   ` json:"is_private"`
 }
 
 type FollowUser struct {
 	FollowUserId int64 `json:"follow_user_id" binding:"required"`
+}
+
+type FollowSuggestions struct {
+	Id         int64  `json:"user_id"`
+	Username   string `json:"username"`
+	FullName   string `json:"full_name"`
+	Email      string `json:"email"`
+	IsFollowed bool   `json:"is_followed_by_them"`
 }
