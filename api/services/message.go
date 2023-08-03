74b2a62d8b0e1b57fbcc18655c096d3b5e52b345
Ashish <dhakalashish18@gmail.com>
2023-08-03 18:34:50 +0545

    feat: services

--- api/services/message.go
+++ api/services/message.go
@@ -1,1 +1,28 @@
 package services
+
+import (
+	"boilerplate-api/api/repository"
+	"boilerplate-api/dtos"
+	"gorm.io/gorm"
+)
+
+type MessageService struct {
+	repository repository.MessageRepository
+}
+
+// NewUserService Creates New user service
+func NewMessageService(repository repository.MessageRepository) MessageService {
+	return MessageService{
+		repository: repository,
+	}
+}
+
+// WithTrx repository with transaction
+func (c MessageService) WithTrx(trxHandle *gorm.DB) MessageService {
+	c.repository = c.repository.WithTrx(trxHandle)
+	return c
+}
+
+func (c MessageService) GetMyConversations(userId int64) ([]dtos.AllConversations, int64, error) {
+	return c.repository.GetMyConversations(userId)
+}
