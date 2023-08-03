c9bc2add3ef1e5b619807bd2a5c9457389fb8742
Ashish <dhakalashish18@gmail.com>
2023-08-03 18:34:56 +0545

    feat: room service

--- api/services/room.go
+++ api/services/room.go
@@ -1,1 +1,28 @@
 package services
+
+import (
+	"boilerplate-api/api/repository"
+	"boilerplate-api/models"
+	"gorm.io/gorm"
+)
+
+type RoomService struct {
+	repository repository.RoomRepository
+}
+
+// NewUserService Creates New user service
+func NewRoomService(repository repository.RoomRepository) RoomService {
+	return RoomService{
+		repository: repository,
+	}
+}
+
+// WithTrx repository with transaction
+func (c RoomService) WithTrx(trxHandle *gorm.DB) RoomService {
+	c.repository = c.repository.WithTrx(trxHandle)
+	return c
+}
+
+func (c RoomService) GetRoomById(Id int64) (models.Rooms, error) {
+	return c.repository.GetRoomById(Id)
+}
