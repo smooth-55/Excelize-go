e93dc019be813801acb12507b5245c658338915e
Ashish <dhakalashish18@gmail.com>
2023-08-03 18:35:31 +0545

    feat: models

--- models/message.go
+++ models/message.go
@@ -4,3 +4,34 @@ type PhoneMessage struct {
 	Phone   string
 	Message string
 }
+
+type Rooms struct {
+	Base
+	Name  string       `gorm:"column:name" json:"name" binding:"required"`
+	Users []*RoomUsers `gorm:"foreignKey:room_id" json:"users"`
+}
+
+func (c Rooms) TableName() string {
+	return "rooms"
+}
+
+type Messages struct {
+	Base
+	RoomId    int64  `gorm:"column:message_room_id" json:"message_room_id" binding:"required"`
+	MessageBy int64  `gorm:"column:message_by" json:"message_by" binding:"required"`
+	Message   string `gorm:"column:message" json:"message" binding:"required"`
+}
+
+func (c Messages) TableName() string {
+	return "messages"
+}
+
+type RoomUsers struct {
+	Base
+	RoomId int64 `gorm:"column:room_id" json:"room_id" binding:"required"`
+	UserId int64 `gorm:"column:user_id" json:"user_id" binding:"required"`
+}
+
+func (c RoomUsers) TableName() string {
+	return "room_users"
+}
