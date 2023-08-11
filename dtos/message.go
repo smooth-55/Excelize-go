9109146e06615bb2822307ef82c9f6c145a7568f
Ashish <dhakalashish18@gmail.com>
2023-08-11 10:19:01 +0545

    feat: add id

--- dtos/message.go
+++ dtos/message.go
@@ -3,6 +3,7 @@ package dtos
 import "boilerplate-api/models"
 
 type AllConversations struct {
+	ID       int                `json:"id"`
 	UserId   int64              `json:"user_id"`
 	Messages []*models.Messages `json:"messages"`
 }
